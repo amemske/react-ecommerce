d72b06c9d8b84d095c9be736774e0c8cbc9ae8e8
Antony Memia <antony@olywave.com>
2020-09-23 23:08:02 +0300

    added product details page, fetching data from api

---
+++ backend/data.js
@@ -0,0 +1,34 @@
+export default {
+
+    products: [
+        {
+        _id: '1',
+        name: 'Slim shirt',
+        category: 'Shirts',
+        image: '/images/d1.png',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews: 10
+    },
+    {
+        _id: '2',
+        name: 'Pink shirt',
+        category: 'Shirts',
+        image: '/images/d1.png',
+        price:80,
+        brand:'Nike',
+        rating:4.1,
+        numReviews: 15
+    },
+    {
+        _id: '3',
+        name: 'Red pants',
+        category: 'Pants',
+        image: '/images/d1.png',
+        price:50,
+        brand:'Nike',
+        rating:4.0,
+        numReviews: 6
+    }]
+}
